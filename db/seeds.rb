23150e6cb0c76db599ebf163b17d3a330a44c6c5
JDumadag25 <justin.dumadag@gmail.com>
2019-07-12 23:12:00 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+owner = User.new
+owner.email = 'owner@owner.com'
+owner.password = 'Owner'
+owner.password_confirmation = 'Owner'
+owner.owner = true
+owner.save
+
+user = User.new
+user.email = 'user@user.com'
+user.password = 'User'
+user.password_confirmation = 'User'
+user.save
