5b9664515954f2392f9ea6a9c178fe4562e16d6f
JDumadag25 <justin.dumadag@gmail.com>
2019-07-30 23:03:21 -0400

    new error

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @user = User.find_by(username: user_login_params[:username])
-  
+    @user = User.find_by(email: user_login_params[:email])
+
     if @user && @user.authenticate(user_login_params[:password])
 
       token = encode_token({ user_id: @user.id })
