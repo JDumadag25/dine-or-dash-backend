6e819eafc733cdb79a1b0f2572bc7b6b03ac33fc
JDumadag25 <justin.dumadag@gmail.com>
2019-08-07 21:07:22 -0400

    user controller edit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,10 +9,11 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new
     @user.email = params[:email]
     @user.password = params[:password]
+    @user.owner = params[:owner]
 
     if (@user.save)
       @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      render json: { user: UserSerializer.new(@user), token: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
