83f3be6bbfb2b9cb885d1ff8828a5570c9b9f13f
JDumadag25 <justin.dumadag@gmail.com>
2019-08-14 23:44:17 -0400

    trying something

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create]
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users, only: [:create]
+  #     post '/login', to: 'auth#create'
+  #     get '/profile', to: 'users#profile'
+  #   end
+  # end
+
+  resources :users
+  resources :profile
 
 
 end
