7c8ebc768668a686f7324eb144891eb8fbd034e3
JDumadag25 <justin.dumadag@gmail.com>
2019-08-20 00:09:43 -0400

    small edit in auth controller, log in works, will continue to test

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: [:create]
-  #     post '/login', to: 'auth#create'
-  #     get '/profile', to: 'users#profile'
-  #   end
-  # end
-
-  resources :users
-  resources :profile
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 
 
 end
