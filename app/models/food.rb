d451abdf42f12310c8f7601313bca8a3aa7627a3
JDumadag25 <justin.dumadag@gmail.com>
2019-09-04 23:33:04 -0400

    setting up restaurant CRUD

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 end
